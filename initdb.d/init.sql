165985b826a1b9a577c87e83d80a0d58a224afab
kenniele <kenniele@yandex.com>
2024-07-25 07:54:40 +0300

    final

--- initdb.d/init.sql
+++ initdb.d/init.sql
@@ -1,23 +1,18 @@
--- Попытка создания пользователя с игнорированием ошибки, если пользователь уже существует
 DO $$
     BEGIN
-        -- Если роль уже существует, она не будет повторно создана
         BEGIN
             EXECUTE 'CREATE ROLE postgres WITH LOGIN SUPERUSER PASSWORD ''12345''';
         EXCEPTION
             WHEN duplicate_object THEN
-                -- Игнорируем ошибку, если роль уже существует
                 RAISE NOTICE 'Role postgres already exists.';
         END;
     END $$;
 
--- Создание таблицы message, если она не существует
 CREATE TABLE IF NOT EXISTS message
 (
     id   SERIAL PRIMARY KEY,
     data TEXT DEFAULT ''::text NOT NULL
 );
 
--- Установка владельца таблицы
 ALTER TABLE message
     OWNER TO postgres;
